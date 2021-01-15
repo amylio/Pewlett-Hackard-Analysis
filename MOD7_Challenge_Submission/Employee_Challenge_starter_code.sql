a6f202ac1c9dfcd54edaac7c14cba0e8f86094bc
Amy Lio <amy.lio@yahoo.com>
2021-01-14 17:58:40 -0800

    Adding and creating submission files

---
+++ MOD7_Challenge_Submission/Employee_Challenge_starter_code.sql
@@ -0,0 +1,9 @@
+SELECT * FROM -- Use Dictinct with Orderby to remove duplicate rows
+SELECT DISTINCT ON (______) _____,
+______,
+______,
+______
+
+INTO nameyourtable
+FROM _______
+ORDER BY _____, _____ DESC;
